e77814e1002aba2ae7d8af03bea5ec72bc2e2cb2
Marko Jerkić <markojerkic266@gmail.com>
2021-11-06 09:53:26 +0100

    initial commit

---
+++ src/main/java/hr/fer/bookexchangeservice/BookExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.bookexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookExchangeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookExchangeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/fer/bookexchangeservice/BookExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.bookexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookExchangeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
