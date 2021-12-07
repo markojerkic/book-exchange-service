65bd501265e8a428fe1c890ee3206f272bdb584a
Marko Jerkić <markojerkic266@gmail.com>
2021-12-07 23:13:53 +0100

    add swagger

--- src/main/java/hr/fer/bookexchangeservice/config/SecurityConfig.java
+++ src/main/java/hr/fer/bookexchangeservice/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -37,11 +38,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilterBefore(this.authTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers(HttpMethod.PUT, "/api/auth").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/auth").authenticated()
                 .antMatchers(HttpMethod.POST, "/api/user").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers(HttpMethod.POST, "/api/**").authenticated()
+                .anyRequest().permitAll();
+
     }
 
     @Override
