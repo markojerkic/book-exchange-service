0761c6d31016af4e3fcd33ae53a25372a87393a2
Marko Jerkić <markojerkic266@gmail.com>
2021-12-29 15:37:45 +0100

    volume for images

--- src/main/java/hr/fer/bookexchangeservice/exception/handeling/MvcExceptionHandler.java
+++ src/main/java/hr/fer/bookexchangeservice/exception/handeling/MvcExceptionHandler.java
@@ -18,55 +18,55 @@ public class MvcExceptionHandler {
     @ExceptionHandler(BadCredentialsException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Korisničko ime ili lozinka nisu točni")
     public void handleBadCredentialsException(HttpServletRequest request, Exception e) {
-        log.info("Bad credentials", e);
+        log.error("Bad credentials", e);
     }
 
     @ExceptionHandler(IsbnAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleIsbnAlreadyExistsException(HttpServletRequest req, Exception e) {
-        log.info("ISBN already exists", e);
+        log.error("ISBN already exists", e);
         return  ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), "isbn"));
     }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleEmailAlreadyExistsException(HttpServletRequest request, Exception e) {
-        log.info("Email already exists", e);
+        log.error("Email already exists", e);
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), "email"));
     }
 
     @ExceptionHandler(UsernameAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleUsernameAlreadyExistsException(HttpServletRequest request, Exception e) {
-        log.info("Username already exists", e);
+        log.error("Username already exists", e);
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), "username"));
     }
 
     @ExceptionHandler(AdvertNotFoundException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Oglas nije pronađen")
     public void handleAdvertNotFound(HttpServletRequest request, Exception e) {
-        log.info("Advert does not exist", e);
+        log.error("Advert does not exist", e);
     }
 
     @ExceptionHandler(ImageNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleImageNotFound(HttpServletRequest request, Exception e) {
-        log.info("Image does not exist", e);
-        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), "image"));
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "SLika nije pronađena")
+    public void handleImageNotFound(HttpServletRequest request, Exception e) {
+        log.error("Image does not exist", e);
     }
 
     @ExceptionHandler(AuthorNotFoundException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Autor nije pronađen")
     public void handleAuthorNotFound(HttpServletRequest request, Exception e) {
-        log.info("Author does not exist", e);
+        log.error("Author does not exist", e);
     }
 
     @ExceptionHandler(BookNotFoundException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Knjiga nije pronađena")
     public void handleBookNotFound(HttpServletRequest request, Exception e) {
-        log.info("Book does not exist", e);
+        log.error("Book does not exist", e);
     }
 
     @ExceptionHandler(GenreNotFoundException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Žanr nije pronađen")
     public void handleGenreNotFound(HttpServletRequest request, Exception e) {
-        log.info("Genre does not exist", e);
+        log.error("Genre does not exist", e);
     }
 
 }
